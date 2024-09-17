841cae96d719724c083ca1d728919a78513a1075
BT_2S14_28 <fdfdsffffff8767@ff>
2024-09-17 11:00:51 -0400

    I added all of my variables and now I just need to print them

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the total bill: ");
+        String bill = s.nextLine();
+        double billNum = Double.parseDouble(bill);
+        System.out.println("Enter the tip percentage as a whole number: ");
+        String tipPercentage = s.nextLine();
+        double tipPercentageNum = Double.parseDouble(tipPercentage);
+        System.out.println("Enter the amount of people: ");
+        String people = s.nextLine();
+        int peopleNum = Integer.parseInt(people);
+        double tipMultiplier = (1+(tipPercentageNum/100));
+        double totalBillWithTip = (billNum*tipMultiplier);
+        double tipAmount = (totalBillWithTip-billNum);
+        double tipPerPerson = (tipAmount/peopleNum);
+
+
 
         }
     }
